f1d6420c5a82fbffa3ec3832f347783c9c010529
sahanashetty1512 <sahana.23cs128@sode-edu.in>
2025-03-14 10:48:40 +0530

    Add files via upload

---
+++ prg18.java
@@ -0,0 +1,21 @@
+package java128;
+class prg18
+{
+    public static void main(String[] args){
+    int age=19;
+    char gender='F';
+    if(gender=='F'){
+     if(age>18){
+         System.out.println("ES");
+     }
+     else{
+         System.out.println(NES);
+     }}
+    else if(gender=='F')
+      {
+       if(age>18){
+System.out.println("EB");
+}else{
+System.out.println("NEB");
+
+}}}}
