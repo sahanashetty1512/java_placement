92239c727d1df91dc9d764612cbd4657b4338072
sahanashetty1512 <sahana.23cs128@sode-edu.in>
2025-03-25 12:48:30 +0530

    Add files via upload

---
+++ prg27.java
@@ -0,0 +1,14 @@
+
+package java128;
+public class prg27 {
+    public static void main(String[] args) {
+        int x=0;
+        do
+        {
+            System.out.println("pune");
+            x++;
+        }
+        while(x<5);
+    }
+    
+}
