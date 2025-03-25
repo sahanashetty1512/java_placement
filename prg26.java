405417f03aa910896ca10158b7aa2ab67f468c82
sahanashetty1512 <sahana.23cs128@sode-edu.in>
2025-03-25 12:40:15 +0530

    Add files via upload

---
+++ prg26.java
@@ -0,0 +1,14 @@
+
+package java128;
+public class prg26 {
+    public static void main(String[] args) {
+        System.out.println("Program starts");
+        int n=5;
+        while(n>0){
+            System.out.println("AI developer");
+            n--;
+        }
+        System.out.println("Program ends");
+    }
+    
+}
