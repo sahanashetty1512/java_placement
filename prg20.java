1d98a00f7e5dc0078d156786b0232eadae24c666
sahanashetty1512 <sahana.23cs128@sode-edu.in>
2025-03-25 11:46:51 +0530

    Add files via upload

---
+++ prg20.java
@@ -0,0 +1,10 @@
+
+package java128;
+public class prg20 {
+    public static void main(String[] args) {
+        for(int k=5;k>=0;k--){
+            System.out.println(k);
+        }
+    }
+    
+}
---
+++ prg21.java
@@ -0,0 +1,11 @@
+pacakge java128;
+class prg21{
+    public static void main(String[] args){
+        int n=5;
+        char ch='e';
+        for(int i=0;i<n;i++){
+            System.out.print(ch+" ");
+            ch--;
+        }
+    }
+}
