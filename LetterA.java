65de23796e957afe5548fdd9a29cd7aa538e31df
sahanashetty1512 <sahana.23cs128@sode-edu.in>
2025-03-27 19:50:25 +0530

    Add files via upload

---
+++ LetterA.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LetterA {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0 || j == n - 1 || i == 0 || i == n / 2)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ LetterE.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class LetterE {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0 || i == 0 || i == n - 1 || i == n / 2)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
