f046e2ab5561aadf5b25e52943d4bc539494f47d
sahanashetty1512 <sahana.23cs128@sode-edu.in>
2025-03-28 09:54:12 +0530

    Add files via upload

---
+++ prg30.java
@@ -0,0 +1,20 @@
+
+package java128;
+
+
+public class prg30 {
+    static int disp(){
+        return 45;
+    }
+    static char print(){
+        return 'a';
+    }
+    public static void main(String[] args) {
+        System.out.println("program starts");
+        int res=disp();
+        System.out.println(res);
+        System.out.println(print());
+        System.out.println("program ends");
+    }
+    
+}
