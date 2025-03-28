61f5ee638dc1ea9fad5de2de38ca600f309e8b51
sahanashetty1512 <sahana.23cs128@sode-edu.in>
2025-03-28 10:13:26 +0530

    Add files via upload

---
+++ prg32.java
@@ -0,0 +1,29 @@
+
+package java128;
+public class prg32 {
+    static void run(){
+        System.out.println("Excuting run()");
+    }
+    static void mul(int a,int b,int c){
+        System.out.println(a*b*c);
+    }
+    static String name(){
+     return "SMVITM";   
+    }
+    static boolean res(int a){
+        if(a%2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+       run();
+       mul(2,3,4);
+       System.out.println(name());
+       boolean res=res(5);
+       System.out.println(res);
+    }
+    
+}
