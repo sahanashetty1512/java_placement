52f9a5fcadf88bc0ad349621e3cb028daef64935
sahanashetty1512 <sahana.23cs128@sode-edu.in>
2025-03-28 10:38:50 +0530

    Add files via upload

---
+++ prg33scanner.java
@@ -0,0 +1,13 @@
+
+package java128;
+import java.util.Scanner;
+public class prg33scanner {
+    public static void main(String[] args) {
+        Scanner sc1=new Scanner(System.in);
+        int a=sc1.nextInt();
+        int b=sc1.nextInt();
+        double c=sc1.nextDouble();
+        System.out.println(a+" "+b+" "+c);
+    }
+    
+}
