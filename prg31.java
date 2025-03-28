8c69894607b4598c5ae465d4869085d61abdc574
sahanashetty1512 <sahana.23cs128@sode-edu.in>
2025-03-28 10:02:05 +0530

    Add files via upload

---
+++ prg31.java
@@ -0,0 +1,18 @@
+
+package java128;
+public class prg31 {
+    static int sq(int a){
+        return a*a;
+    }
+    static int cube(int b){
+        int ans=b*b*b;
+        return ans;
+    }
+    public static void main(String[] args) {
+        System.out.println("program starts");
+        System.out.println(sq(2));
+        System.out.println(cube(3));
+        System.out.println("program ends");
+    }
+    
+}
