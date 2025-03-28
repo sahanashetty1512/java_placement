84eb22c5a1fdd7a765c88537d41b69b56db6bdc5
sahanashetty1512 <sahana.23cs128@sode-edu.in>
2025-03-28 09:46:31 +0530

    Add files via upload

---
+++ prg28.java
@@ -0,0 +1,12 @@
+package java128;
+public class prg28 {
+    static void greetings(){
+        System.out.println("Happy morning");
+    }
+    public static void main(String[] args) {
+      System.out.println("Program starts");
+      greetings();
+      System.out.println("program ends");
+    }
+    
+}
---
+++ prg29.java
@@ -0,0 +1,19 @@
+
+package java128;
+
+public class prg29 {
+
+    static void add(int a,int b)
+    {
+        System.out.println("Sum of a+b:"+(a+b));
+    }
+    public static void main(String[] args) {
+        System.out.println("Program strts");
+        add(10,2);
+        add(4,6);
+        add(3,3);
+        System.out.println("program ends");
+        
+    }
+    
+}
