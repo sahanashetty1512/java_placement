0f005869a3ad5c56cebd40692c4679e1f365be1c
sahanashetty1512 <sahana.23cs128@sode-edu.in>
2025-04-08 12:16:25 +0530

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,11 @@
+
+public class Array2 {
+    public static void main(String[] args) {
+        char[] vowels={'a','e','i','o','u'};
+        for(int i=0;i<vowels.length;i++)
+        {
+            System.out.println(vowels[i]);
+        }
+    }
+    
+}
