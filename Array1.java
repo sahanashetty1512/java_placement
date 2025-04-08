d671ad2031508fd5163df93bb19572ce8a3c8730
sahanashetty1512 <sahana.23cs128@sode-edu.in>
2025-04-08 11:29:40 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,18 @@
+package java128;
+public class Array1 {
+    public static void main(String[] args) {
+        int[] arr=new int[5];
+        arr[0]=11;
+        arr[1]=22;
+        arr[2]=33;
+        arr[3]=44;
+        arr[4]=55;
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+        
+    }
+    
+}
